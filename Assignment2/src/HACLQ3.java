04b6da8c8311d9c5641e65bfcc91f116844605a7
Labores olis <122188954+laboressolis@users.noreply.github.com>
2024-12-01 23:11:07 +0530

    Command Line Home Assignment 1,2,3

---
+++ Assignment2/src/HACLQ3.java
@@ -0,0 +1,10 @@
+/*Write a java program to input a four-digit number from command line argument and find sum
+of the first and last digit of the number*/
+public class HACLQ3 {
+	public static void main(String[] args) {
+		int digit = Integer.parseInt(args[0]);
+		int firstDigit = digit / 1000;
+		int secondDigit = digit % 10;
+		System.out.println("The sum of firstdigit and lastdigit is "+(firstDigit+secondDigit));
+	}
+}
