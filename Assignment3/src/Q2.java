c5e4e6fa3a142cc1d9dd7d4a488565ccc73b783e
Labores olis <122188954+laboressolis@users.noreply.github.com>
2024-12-02 20:17:08 +0530

    Assigment 3 Question - 6 & 7

--- Assignment3/src/Q2.java
+++ Assignment3/src/Q2.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Q2 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
@@ -6,8 +7,7 @@ public class Q2 {
 		int ml = input.nextInt();
 		if (ml >= 5000) {
 			System.out.println("Yes, Alice is following doctor’s advice");
-		}
-		else {
+		} else {
 			System.out.println("No, Alice is not following doctor’s advice");
 		}
 	}
