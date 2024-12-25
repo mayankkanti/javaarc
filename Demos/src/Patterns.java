4cd21d3ee4b9d6308dcbc3cde00d9f5c075b47a7
Labores olis <122188954+laboressolis@users.noreply.github.com>
2024-12-25 19:08:02 +0800

    CaesarCipher and Patterns

--- Demos/src/Patterns.java
+++ Demos/src/Patterns.java
@@ -1,15 +1,40 @@
 public class Patterns {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static void pattern1(int rows) {
+		System.out.println("Pattern 1");
+		System.out.println();
+		
 		boolean placeHolder = true;
 		String line, temp;
 		line = "";
-		for (int i = 0; i < 4; i++) {
-			temp = placeHolder ? "1" : "0"; // basically if else, condition ? valueifTrue : valueifFalse;
-			// alternative if placeHolder == true, return "1" else return "2"
+		for (int i = 0; i < rows; i++) {
+			temp = placeHolder ? "1" : "0";
 			line = temp + line;
 			placeHolder = !placeHolder;
-			System.out.println("When i="+i+" placeHolder="+placeHolder+" temp="+temp+" line="+line);
+			System.out.println(line);
 		}
+	}
+	
+	static void pattern2(int rows) {
+		System.out.println("Pattern 2");
+		System.out.println();
+		
+		for (int i=0; i < rows; i++) {
+			for (int j=rows; j>i;j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+	
+	static void pattern3(int rows) {
+		System.out.println("Pattern 3");
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		int n = 4;
+		pattern1(n);
+		pattern2(n);
+		pattern3(n);
 	}
 }
--- Demos/src/testing.java
+++ Demos/src/testing.java
@@ -2,8 +2,8 @@
 public class testing {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a = 97;
+		System.out.println((char)a);
 	}
 
 }
