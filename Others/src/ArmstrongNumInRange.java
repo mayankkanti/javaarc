d1bc906d46132949db5c30bb225894440468da59
Labores olis <122188954+laboressolis@users.noreply.github.com>
2024-12-26 02:13:48 +0800

    Finding Prime Numbers and Armstrong Numbers in (a,b) range

---
+++ Others/src/ArmstrongNumInRange.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class ArmstrongNumInRange {
+	static boolean isArmstrong(int num) {
+		int newNum = 0, q=num, bit;
+	       int digits = 0, temp = num;
+	        while (temp != 0) {
+	            digits++;
+	            temp /= 10;
+	        }
+		
+		while (q!=0) {
+			bit = q%10;
+			newNum += Math.pow(bit,digits);
+			q = q/10;
+		}
+		if (newNum == num) {
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the range val1: ");
+		int num1 = sc.nextInt();
+		System.out.print("Enter the range val2: ");
+		int num2 = sc.nextInt();
+		for (int num = num1; num <=num2; num++) {
+			if (isArmstrong(num)) {
+				System.out.println(num);
+			}
+		}
+	}
+
+}
--- Others/src/GCD.java
+++ Others/src/GCD.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class GCD {
 
 	public static void main(String[] args) {
@@ -7,21 +8,22 @@ public class GCD {
 		int num1 = sc.nextInt();
 		System.out.print("Enter the second Number: ");
 		int num2 = sc.nextInt();
-		
+
 		int divisor = Math.min(num1, num2);
 		int dividend = Math.max(num1, num2);
-		
+
 		int temp = 1;
-		
+
 		while (temp != 0) {
-			temp = dividend%divisor;
+			temp = dividend % divisor;
 			dividend = divisor;
 			divisor = temp;
-			//System.out.println(String.format("[Debug] Temp: %d Dividend: %d Divisor: %d", temp, dividend, divisor));
+			// System.out.println(String.format("[Debug] Temp: %d Dividend: %d Divisor: %d",
+			// temp, dividend, divisor));
 		}
-		
-		System.out.println("The GCD of the numbers "+num1+" and "+num2+" is "+dividend);
-		
+
+		System.out.println("The GCD of the numbers " + num1 + " and " + num2 + " is " + dividend);
+
 	}
 
 }
