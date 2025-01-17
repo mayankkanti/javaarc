85bd67037780617f0a2f9d7b5002e6d97dfbff60
Labores olis <122188954+laboressolis@users.noreply.github.com>
2025-01-17 20:18:51 +0530

    Assignment 6 HAQ3-HAQ5

---
+++ Assignment6/src/HAQ3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class HAQ3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a string: ");
+		String user = sc.nextLine();
+		System.out.println("The middle character in the string: "+user.charAt((user.length()/2)));
+	}
+
+}
