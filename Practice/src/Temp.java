9d62b1f130df2d5810a5b05dd80b570833241c7c
Labores olis <122188954+laboressolis@users.noreply.github.com>
2025-01-17 17:53:33 +0530

    Assignment 6 HA Q1-2

--- Practice/src/Temp.java
+++ Practice/src/Temp.java
@@ -3,43 +3,42 @@ public class Temp {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = 1;
-		System.out.print("Enter the angle to be calculated: ");
+		// Angle to be calculated.
+		System.out.println("Enter an angle(x): ");
 		double x = sc.nextDouble();
+		x = x%(2*Math.PI);
+		// Value of n.. (odd increment)
+		int n = 1;
+		// Sign of each term (alternating)
+		int sign = 1;
+		// Sum of each term (final result to be compared)
+		double sum = 0;
 		
-		x = x % (2* Math.PI);
-		double sign = 1.0;
-		double sum = 0.0d;
 		while (true) {
-			// Resetting Factorial for each iteration
+			// factorial gets reset every iteration.
 			double factorial = 1;
-	
-			// Calculating factorial
+			// Calculating Factorial
 			for (int i=1; i<=n; i++) {
-				factorial *= i;
+				factorial*=i;
 			}
-			
-			// Calculating term
-			double term = sign*(Math.pow(x,n)/factorial);
-			
-			// Calculating sum
+			// Calculating Term and Adding to sum.
+			double term = sign*(Math.pow(x, n)/factorial);
 			sum += term;
-			
-			System.out.printf("[Debug] x:%f n:%d factorial:%f term:%f sum:%f",x,n,factorial, term, sum);
-			System.out.println();
-			
-			// Termination Condition
+			// Termination condition i.e Term should be less than 10^-6
 			if (Math.abs(term)<Math.pow(10, -6)) {
 				break;
 			}
+			// Debug Statements
+			//System.out.printf("x:%f n:%d factorial:%f term:%f sum:%f",x,n,factorial,term,sum);
+			//System.out.println();
+
 			
-			// Increments and Sign Changes
-			n+=2;
-			sign = sign * -1;
-			
+			// Increments and sign change
+			sign *= -1;
+			n += 2;
 		}
-		System.out.println("Calculated Sin(x): "+sum);
-		System.out.println("System Sin(x): "+Math.sin(x));
+		System.out.println("Calculated Result: "+sum);
+		System.out.println("System Result: "+Math.sin(x));
 	}
 
 }
