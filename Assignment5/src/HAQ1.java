9feb5b6ec83a5489052556c775fb54bf6f02b6f8
Labores olis <122188954+laboressolis@users.noreply.github.com>
2025-01-28 19:19:07 +0530

    Fixed Assignment5 HAQ1 and HAQ2

--- Assignment5/src/HAQ1.java
+++ Assignment5/src/HAQ1.java
@@ -3,16 +3,14 @@ public class HAQ1 {
 
 	public static void main(String[] args) {
 		int n = 5;
-		int count = 1;
 		for (int i = 1; i <= n; i++) {
 			for (int j = n; j >= 1; j--) {
-				if (count == j) {
+				if (i == j) {
 					System.out.print("*");
 				} else {
 					System.out.print(j);
 				}
 			}
-			count++;
 			System.out.println();
 
 		}
