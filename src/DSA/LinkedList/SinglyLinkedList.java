f477fdca3e1c1925a45cc3627b4de4fe26b45c61
Mayank <mayankkanti2325@gmail.com>
2025-05-10 13:04:44 +0530

    Add SinglyLinkedList

---
+++ src/DSA/LinkedList/SinglyLinkedList.java
@@ -0,0 +1,264 @@
+package DSA.LinkedList;
+
+import java.lang.Thread;
+import java.util.Random;
+
+class Node {
+    String data;
+    Node next;
+    
+    Node(String data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    void insertAtBeginning(String data){
+        Node newNode = new Node(data);
+        if (head == null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    void insertAtEnd(String data) {
+        Node newNode = new Node(data);
+        if (head == null){
+            head = newNode;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        current.next = newNode;
+    }
+
+    void insertAtIndex(int index, String data){
+        Node newNode = new Node(data);
+        if (index == 0){
+            insertAtBeginning(data);
+            return;
+        }
+        Node current = head;
+        Node previous = null;
+        for(int i = 0; i < index; i++){
+            if (current == null){
+                System.out.println("Index out of bounds.");
+                return;
+            }
+
+            previous = current;
+            current = current.next;
+        }
+
+        previous.next = newNode;
+        newNode.next = current;
+
+    }
+    
+    void deleteAtBeginning(){
+        if (head == null){
+            System.out.println("The List is Empty.");
+            return;
+        }
+        head = head.next;
+    }
+    
+    void deleteAtEnd() {
+        if (head == null){
+            System.out.println("The List is Empty.");
+            return;
+        }
+
+        Node current = head;
+        Node previous = null;
+        while (current.next != null){
+            previous = current;
+            current = current.next;
+        }
+
+        previous.next = null;
+
+    }
+    
+    void deleteAtIndex(int index) {
+        if (head == null){
+            System.out.println("The List is Empty.");
+            return;
+        }
+
+        Node current = head;
+        Node previous = null;
+
+        for(int i = 0; i < index; i++){
+            if (current == null){
+                System.out.println("Index out of bounds.");
+                return;
+            }
+            
+            previous = current;
+            current = current.next;
+        }
+
+        previous.next = current.next;
+    }
+    
+    void searchByData(String data) {
+        if (head == null){
+            System.out.println("The List is Empty.");
+            return;
+        }
+
+        Node current = head;
+        int index = 0;
+        boolean found = false;
+
+        while (current!= null){
+            if (current.data.equals(data)){
+                found = true;
+                System.out.printf("Found %s at index %d \n", data, index);
+            }
+            current = current.next;
+            index++;
+        }
+
+        if (!found){
+            System.out.printf("No data found with value %s \n", data);
+        }
+    }
+    
+    void searchByIndex(int index) {
+        if (head == null){
+            System.out.println("The List is Empty.");
+            return;
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++){
+            if (current == null){
+                System.out.println("Index out of bounds.");
+                return;
+            }
+            current = current.next;
+        }
+
+        System.out.printf("Data at Index %d is %s \n", index, current==null?null:current.data);
+    }
+
+    void updateByIndex(int index, String data) {
+        if (head == null){
+            System.out.println("The List is Empty.");
+            return;
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++){
+            if (current.next == null){
+                System.out.println("Index out of bounds.");
+                return;
+            }
+            current = current.next;
+        }
+        System.out.printf("Update data at index %d from %s to %s \n", index, current.data, data);
+        current.data = data;
+        
+    }
+
+    void displayList() {
+        Node current = head;
+        System.out.println("Current Linked List: ");
+        while(current.next != null){
+            System.out.print(current.data + " > ");
+            current = current.next;
+        }
+        System.out.println(current.data + " > Null \n");
+    }
+
+}
+
+public class SinglyLinkedList {
+
+    static void insertOperations(LinkedList list, int OperationDelay) throws InterruptedException{
+        System.out.println("Inserting 10 at beginning. \n");
+        Thread.sleep(OperationDelay);
+        list.insertAtBeginning("10");
+        list.displayList();
+
+        System.out.println("Inserting 22 at end. \n");
+        Thread.sleep(OperationDelay);
+        list.insertAtEnd("22");
+        list.displayList();
+
+        System.out.println("Inserting 69 at index 6. \n");
+        Thread.sleep(OperationDelay);
+        list.insertAtIndex(6, "69");
+        list.displayList();
+    }
+
+    static void deleteOperations(LinkedList list, int OperationDelay) throws InterruptedException{
+        System.out.println("Deleting Node at Beginning. \n");
+        Thread.sleep(OperationDelay);
+        list.deleteAtBeginning();
+        list.displayList();
+
+        System.out.println("Deleting Node at End. \n");
+        Thread.sleep(OperationDelay);
+        list.deleteAtEnd();
+        list.displayList();
+
+        System.out.println("Deleting Node at Index 2. \n");
+        Thread.sleep(OperationDelay);
+        list.deleteAtIndex(2);
+        list.displayList();
+    }
+
+    static void searchOperations(LinkedList list, int OperationDelay) throws InterruptedException{
+        System.out.println("Searching by data value 69");
+        Thread.sleep(OperationDelay);
+        list.searchByData("69");
+        list.displayList();
+
+        System.out.println("Searching data at index 5");
+        Thread.sleep(OperationDelay);
+        list.searchByIndex(5);
+        list.displayList();
+    }
+    
+    static void updateOperations(LinkedList list, int OperationDelay) throws InterruptedException{
+        System.out.println("Updating data at index 2 to 55");
+        Thread.sleep(OperationDelay);
+        list.updateByIndex(2, "55");
+        list.displayList();
+    }
+    public static void main(String[] args) {
+        
+
+        Random random = new Random();
+        int OperationDelay = 2500;
+
+        try {
+            LinkedList list = new LinkedList();
+
+            System.out.println("Generating a Random Linked List. \n");
+            Thread.sleep(OperationDelay);
+            for (int i = 0; i < 5; i++){
+                list.insertAtEnd(Integer.toString(random.nextInt(20)));
+            }
+            list.displayList();
+
+            // insertOperations(list, OperationDelay);
+            // deleteOperations(list, OperationDelay);
+            // searchOperations(list, OperationDelay);
+            updateOperations(list, OperationDelay);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }    
+}
