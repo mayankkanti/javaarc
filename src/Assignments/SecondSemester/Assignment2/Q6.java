1cc1eb5b3bb0fee7b92f1fb743cd1b81d28d731d
Mayank <mayankkanti2325@gmail.com>
2025-05-10 10:52:08 +0530

    Add Assignment2 Q6

--- src/Assignments/SecondSemester/Assignment2/Q6.java
+++ src/Assignments/SecondSemester/Assignment2/Q6.java
@@ -1,5 +1,35 @@
 package Assignments.SecondSemester.Assignment2;
 
+class Vehicle {
+    String model;
+    int year;
+
+    Vehicle(String model, int year) {
+        this.model = model;
+        this.year = year;
+    }
+}
+
+class car extends Vehicle {
+    int CarID;
+    double price;
+    car(String model, int year, int CarID, double price) {
+        super(model, year);
+        this.CarID = CarID;
+        this.price = price;
+    }
+    void displayDetails() {
+        System.out.println("Car ID: " + CarID);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+        System.out.println("Price: " + price);
+    }
+}
+
 public class Q6 {
+    public static void main(String[] args) {
+        car obj1Car = new car("Toyota", 2020, 101, 20000.0);
+        obj1Car.displayDetails();
+    }
     
 }
